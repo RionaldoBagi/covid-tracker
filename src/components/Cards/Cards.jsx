351ea089d5630310a36243a4851f0807bc399204
Rionaldo G Pieterson Bagi <64066988+RionaldoBagi@users.noreply.github.com>
2020-12-08 11:40:43 +0800

    Tampilan cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card,CardTitle} from "reactstrap";
 import styles from "./Cards.module.css";
+import NumberFormat from "react-number-format";
 
 const Cards = ({ data: { confirmed, recovered, deaths }, country }) => {
   if (!confirmed) {
@@ -11,17 +12,31 @@ const Cards = ({ data: { confirmed, recovered, deaths }, country }) => {
     <div className={styles.container}>
       <br />
       <h2>{country ? `Negara: ${country}` : `Global`}</h2>
-      <Card body className="text-center">
+      <Card body className="text-center"
+      style= {{borderRadius: 20, backgroundColor: "Grey"}}>
         <CardTitle><h5>Positif</h5></CardTitle>
-        <h5>{confirmed.value}</h5>
+        <h5>
+        <NumberFormat
+          value={confirmed.value}
+          thousandSeparator={true}
+          displayType={"text"}/>
+          </h5>
       </Card>
-      <Card body className="text-center">
+      <Card body className="text-center"
+      style= {{borderRadius: 20, backgroundColor: "Green"}}>
         <CardTitle><h5>Sembuh</h5></CardTitle>
-        <h5>{recovered.value}</h5>
+        <h5><NumberFormat
+          value={confirmed.value}
+          thousandSeparator={true}
+          displayType={"text"}/></h5>
       </Card>
-      <Card body className="text-center">
+      <Card body className="text-center"
+      style= {{borderRadius: 20, backgroundColor: "Red"}}>
         <CardTitle><h5>Meninggal</h5></CardTitle>
-        <h5>{deaths.value}</h5>
+        <h5><NumberFormat
+          value={confirmed.value}
+          thousandSeparator={true}
+          displayType={"text"}/></h5>
       </Card>
     </div>
   );
