bd4eb48244271b3b1e04e08e4ccc010e5d4c974e
Rionaldo G Pieterson Bagi <64066988+RionaldoBagi@users.noreply.github.com>
2020-12-08 12:33:05 +0800

    Tampilan Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Card,CardTitle} from "reactstrap";
 import styles from "./Cards.module.css";
 import NumberFormat from "react-number-format";
 
@@ -12,6 +13,12 @@ const Cards = ({ data: { confirmed, deaths, recovered }, text }) => {
       <div className={styles.row}>
         <div className={styles.column}>
           <div className={styles.positif}>
+            <Card
+            body
+            className="text-center"
+            inverse
+            style={{ borderRadius: 25, backgroundColor: "Grey" }}
+          >
             <h3>Positif</h3>
             <p>
               <NumberFormat
@@ -20,10 +27,17 @@ const Cards = ({ data: { confirmed, deaths, recovered }, text }) => {
                 displayType={"text"}
               />
             </p>
+            </Card>
           </div>
         </div>
         <div className={styles.column}>
           <div className={styles.meninggal}>
+          <Card
+            body
+            className="text-center"
+            inverse
+            style={{ borderRadius: 25, backgroundColor: "Red" }}
+          >
             <h3>Meninggal</h3>
             <p>
               <NumberFormat
@@ -32,10 +46,17 @@ const Cards = ({ data: { confirmed, deaths, recovered }, text }) => {
                 displayType={"text"}
               />
             </p>
+            </Card>
           </div>
         </div>
         <div className={styles.column}>
           <div className={styles.sembuh}>
+          <Card
+            body
+            className="text-center"
+            inverse
+            style={{ borderRadius: 25, backgroundColor: "Lime" }}
+          >
             <h3>Sembuh</h3>
             <p>
               <NumberFormat
@@ -44,6 +65,7 @@ const Cards = ({ data: { confirmed, deaths, recovered }, text }) => {
                 displayType={"text"}
               />
             </p>
+            </Card>
           </div>
         </div>
       </div>
